f6b4f5889fc2467305ddd938b4bb247f1dfdd617
Aleksandr Dekunov <wresda@gmail.com>
2023-10-13 17:49:42 +0300

    fd

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,3 +1,4 @@
+import pytest
 from configuration import *
 
 
@@ -9,5 +10,15 @@ def test_slogen():
     assert 1 + 3 == 4, "Правильно"
 
 
+class Street():
+    def test_solo():
+        assert SON + NAME == 7, "Правильно"
+
 def test_solo():
-    assert SON + NAME == 4, "Правильно"
+    assert Street == 7, "Правильно"
+
+
+@pytest.mark.parametrize("test_input,expected", [("3+5", 8), ("2+4", 6), ("6*9", 42)])
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
+
